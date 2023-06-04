a350fa8068ab94ef82ff4a137e5b57fe44c4f787
Jacob Hunt <jacobhuntdevelopment@gmail.com>
2023-06-03 23:47:25 -0400

    Alphabetize test includes

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -15,11 +15,11 @@
 #include "../Libraries/Catch2/catch.hpp"
 
 // Include all unit tests for all collections in the project
-#include "../Lists/SinglyLinkedList/Tests/GetElementAtIndexTests.cpp"
-#include "../Lists/SinglyLinkedList/Tests/GetLengthTests.cpp"
-#include "../Lists/SinglyLinkedList/Tests/GetFirstIndexOfTests.cpp"
-#include "../Lists/SinglyLinkedList/Tests/InsertAtIndexTests.cpp"
-#include "../Lists/SinglyLinkedList/Tests/DeleteFromIndexTests.cpp"
-#include "../Lists/SinglyLinkedList/Tests/UpdateAtIndexTests.cpp"
 #include "../Lists/SinglyLinkedList/Tests/ClearTests.cpp"
+#include "../Lists/SinglyLinkedList/Tests/DeleteFromIndexTests.cpp"
 #include "../Lists/SinglyLinkedList/Tests/DestructorTests.cpp"
+#include "../Lists/SinglyLinkedList/Tests/GetElementAtIndexTests.cpp"
+#include "../Lists/SinglyLinkedList/Tests/GetFirstIndexOfTests.cpp"
+#include "../Lists/SinglyLinkedList/Tests/GetLengthTests.cpp"
+#include "../Lists/SinglyLinkedList/Tests/InsertAtIndexTests.cpp"
+#include "../Lists/SinglyLinkedList/Tests/UpdateAtIndexTests.cpp"
